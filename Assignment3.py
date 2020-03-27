67bb3aa5529ae70bf5072f033b0f39df9cbfe002
Iretiayomide <60120083+Iretiayomide@users.noreply.github.com>
2020-03-26 23:50:51 -0400

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'Anaconda2/salesdata.db'
+engine = create_engine(r"sqlite:///{}"
+.format(db_file))
+
+sql = 'SELECT * from scores'
+sales_data_df = pd.read_sql(sql, engine)       
+sales_data_df.head()
+
