f57eec1f9fdf400633c4cb3e3df224b64ede2ed1
Iretiayomide <60120083+Iretiayomide@users.noreply.github.com>
2020-04-02 11:38:58 -0400

    Update Assignment 4a.py

--- Assignment 4a.py
+++ Assignment 4a.py
@@ -17,10 +17,6 @@ bins = [0, 60, 70, 80, 90, 100]
 #create names for the groups
 group_names = ['F', 'D', 'C', 'B', 'A']
 
-#create pass_or_fail
-#status = ['Fail', 'Fail', 'Fail','Pass', 'Pass']
-
-
 # categorize column grades based on bins
 df['lettergrade'] = pd.cut(df['grade'], bins, labels = group_names)
 df['status'] = np.where(df['grade']>80,'pass', 'fail')
